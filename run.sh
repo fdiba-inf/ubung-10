9dbac6ca09b5fac4264ed687301b6b050b78fc68
stef2georg <stefan.georgiev91@gmail.com>
2020-12-01 00:55:24 +0200

    Übung 10

--- run.sh
+++ run.sh
@@ -20,8 +20,8 @@ read -p 'Enter index: ' enteredIndex
 index=0
 for file in exercise*/*Demo.java; do
 	if [[ "$index" == "$enteredIndex" ]]; then
-		echo 'javac exercise9/Utils.java exercise9/Point.java exercise9/PointDemo.java exercise9/Rectangle.java exercise9/RectangleDemo.java exercise9/Ellipse.java exercise9/EllipseDemo.java'
-		javac exercise9/Utils.java exercise9/Point.java exercise9/PointDemo.java exercise9/Rectangle.java exercise9/RectangleDemo.java exercise9/Ellipse.java exercise9/EllipseDemo.java
+		echo 'javac exercise10/Utils.java exercise10/Point.java exercise10/PointDemo.java exercise10/ClickListener.java exercise10/Figure.java exercise10/Rectangle.java exercise10/RectangleDemo.java exercise10/Ellipse.java exercise10/EllipseDemo.java exercise10/ClickDemo.java'
+		javac exercise10/Utils.java exercise10/Point.java exercise10/PointDemo.java exercise10/ClickListener.java exercise10/Figure.java exercise10/Rectangle.java exercise10/RectangleDemo.java exercise10/Ellipse.java exercise10/EllipseDemo.java exercise10/ClickDemo.java
 		class=$(echo $file | sed 's/.java//' | sed 's/\//./')
 		echo "java -ea $class"
 		echo -e "\U0001f680 \U0001f648 \U0001f649 \U0001f64A \U0001f680"
